b5dd5a2c4bc838b9ad29200c74d276d792cc9f68
baibaoming <baibaoming@58.com>
2025-03-13 17:04:53 +0800

    temp ci

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function btn_onclick() {
     var btn = document.getElementsByName("b1")[0];
     console.log(btn.value);
-    
+
     var str = getWVRChatConfig();
     // 简化条件判断，直接赋值
     btn.value = str;
-    
+
     // 创建或更新图片元素
     var img = document.getElementById('dynamicImage');
     if (!img) {
@@ -14,5 +14,5 @@ function btn_onclick() {
         img.style.maxWidth = '100%';
         document.body.appendChild(img);
     }
-    img.src = 'https://example.com/your-image.jpg'; // 替换为你的实际图片URL
+    img.src = 'wvrhttp://photo2.png'; // 替换为你的实际图片URL
 }
